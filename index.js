e4c28e590eb1e543c1983772197049dafa539a82
Guilherme Miorelli <igusgu@hotmail.com>
2023-08-17 16:15:47 -0300

    código melhor comentado

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 //requisitando a biblioteca lodash
 const { uniq } = require("lodash");
-//requisitando o arquivo externo
+//requisitando o arquivo externo e desestruturando os objetos arrays
 const { arrayNumeros, arrayLetras } = require("./array.js");
-//apresentar elementos sem repeti-los
+//apresentar elementos sem repeti-los e armazenando em uma variável
 const arrayNumeroUnico = uniq(arrayNumeros);
 const arrayLetrasUnico = uniq(arrayLetras);
-
+//apresentando os valores filtrados
 console.log(arrayLetrasUnico);
 console.log(arrayNumeroUnico);
